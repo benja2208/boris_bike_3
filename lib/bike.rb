d289091643f99ab00c29f735d8a9d2f67b416dc9
Sarah Middleton <middleton.sarahlouise@gmail.com>
2015-07-23 11:28:44 +0100

    Made report_broken and broken? work together

--- lib/bike.rb
+++ lib/bike.rb
@@ -1,14 +1,19 @@
 class Bike
 
+def initialize
+  @broken = false
+end
+
 def working?
 	true
 end
 
 def report_broken
-
-end 
+  @broken = true
+end
 
 def broken?
+  @broken
 end
 
 end
--- spec/bike_spec.rb
+++ spec/bike_spec.rb
@@ -7,7 +7,16 @@ describe Bike do
   expect(subject).to respond_to :broken?
  end
 
- it "should respond to method report_broken" do 
+ it "should respond to method report_broken" do
  	expect(subject).to respond_to :report_broken
- end 
+ end
+
+ describe "#broken?" do
+  it "should return true if a bike is reported broken" do
+    subject.report_broken
+    expect(subject.broken?).to eq true
+  end
+
+ end
+
 end
